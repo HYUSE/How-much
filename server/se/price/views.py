61b7a6ebac5b78616e5a64304c9824f9fa1e4b8a
hari <hari@hariui-MacBook-Air.local>
2015-05-04 20:57:58 +0900

    remove grade in result data

--- server/se/price/views.py
+++ server/se/price/views.py
@@ -39,7 +39,7 @@ def index(request):
 				for item in items:
 					price_date = str(item.price_date)
 					price_date = price_date[0:4]+price_date[5:7]+price_date[8:]
-					price.append(dict(grade=item.category.name, price_r=item.price_r, price_w=item.price_w, unit_r=item.unit_r, unit_w=item.unit_w, date=price_date))
+					price.append(dict(price_r=item.price_r, price_w=item.price_w, unit_r=item.unit_r, unit_w=item.unit_w, date=price_date))
 				if len(price) != 0:
 					response_j["data"].append(dict(grade=ssub.name, price=price))
 		return HttpResponse(json.dumps(response_j), content_type="application/json")
